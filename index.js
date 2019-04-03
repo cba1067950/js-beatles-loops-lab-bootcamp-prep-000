b205c48e7e263a3bb2f74e9a390b8cd12028e396
Joe Jamsky <cba1067950@yahoo.com>
2019-04-03 03:16:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians, instruments){
   var list = [];
   
-  for(i = 0; i < musicians.length; i++){
+  for(var i = 0; i < musicians.length; i++){
     list.push(`${musicians[i+1]} plays ${instruments[i+1]}`)
   }
   return list;
